7c60a12b7f2f7ec8c18ff0e6e6274883820ac539
Marjan Hrzic <marjan.hrzic@gmail.com>
2012-11-12 09:33:42 +0000

    Prebaceno na GraphScene<Node,String>

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/ClassDiagramContainerWidget.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/ClassDiagramContainerWidget.java
@@ -11,7 +11,6 @@ import org.netbeans.api.visual.action.AcceptProvider;
 import org.netbeans.api.visual.action.ActionFactory;
 import org.netbeans.api.visual.action.ConnectorState;
 import org.netbeans.api.visual.border.BorderFactory;
-import org.netbeans.api.visual.layout.LayoutFactory;
 import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.widget.general.IconNodeWidget;
 import org.openide.util.Exceptions;
@@ -22,25 +21,25 @@ import org.uml.model.UmlClassElement;
  *
  * @author Uros
  */
-public class ClassDiagramWidget extends IconNodeWidget {
+public class ClassDiagramContainerWidget extends IconNodeWidget {
 
     private UmlClassDiagram umlClassDiagram;
 
-    public ClassDiagramWidget(UmlClassDiagram umlClassDiagram, ClassDiagramScene scene) {
+    public ClassDiagramContainerWidget(UmlClassDiagram umlClassDiagram, ClassDiagramScene scene) {
         super(scene);
         this.umlClassDiagram = umlClassDiagram;
-        
+      //  setLayout(new AbsoluteLayout());
         //setLayout(LayoutFactory.createVerticalFlowLayout(LayoutFactory.SerialAlignment.CENTER, 50));
         setBorder(BorderFactory.createLineBorder(4, Color.black));
+        getActions().addAction(ActionFactory.createMoveAction());
         getActions().addAction(ActionFactory.createAcceptAction(new AcceptProvider() {
             @Override
             public ConnectorState isAcceptable(Widget widget, Point point, Transferable t) {
                 return ConnectorState.ACCEPT;
             }
-
             @Override
             public void accept(Widget widget, Point point, Transferable t) {
-                Class<? extends UmlClassElement> droppedClass = (Class<? extends UmlClassElement>) t.getTransferDataFlavors()[2].getRepresentationClass();
+                Class<? extends UmlClassElement> droppedClass = (Class<? extends UmlClassElement>) t.getTransferDataFlavors()[2].getRepresentationClass(); // Jako ruzno! Osmisliti kako da izvlacimo iz DataFlavor-a bez gadjanja indeksa!
                 try {
                     addChild(new ClassWidget((ClassDiagramScene) getScene(), (UmlClassElement) droppedClass.newInstance()));
                 } catch (InstantiationException ex) {
