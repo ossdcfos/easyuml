ff0c3c0053899f7f9b908142389e6c7eb4d82a74
Marjan Hrzic <marjan.hrzic@gmail.com>
2012-11-13 23:14:00 +0000

    - Dodata resize funkcionalnost

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/ClassDiagramContainerWidget.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/ClassDiagramContainerWidget.java
@@ -5,12 +5,17 @@
 package org.uml.visual.widgets;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.datatransfer.Transferable;
 import org.netbeans.api.visual.action.AcceptProvider;
 import org.netbeans.api.visual.action.ActionFactory;
 import org.netbeans.api.visual.action.ConnectorState;
 import org.netbeans.api.visual.border.BorderFactory;
+import org.netbeans.api.visual.graph.GraphScene;
+import org.netbeans.api.visual.layout.LayoutFactory;
+import org.netbeans.api.visual.widget.LabelWidget;
+import org.netbeans.api.visual.widget.LayerWidget;
 import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.widget.general.IconNodeWidget;
 import org.openide.util.Exceptions;
@@ -21,22 +26,26 @@ import org.uml.model.UmlClassElement;
  *
  * @author NUGS
  */
-public class ClassDiagramContainerWidget extends IconNodeWidget {
+public class ClassDiagramContainerWidget extends Widget {
 
     private UmlClassDiagram umlClassDiagram;
 
     public ClassDiagramContainerWidget(UmlClassDiagram umlClassDiagram, ClassDiagramScene scene) {
         super(scene);
         this.umlClassDiagram = umlClassDiagram;
-      //  setLayout(new AbsoluteLayout());
-        //setLayout(LayoutFactory.createVerticalFlowLayout(LayoutFactory.SerialAlignment.CENTER, 50));
-        setBorder(BorderFactory.createLineBorder(4, Color.black));
-        getActions().addAction(ActionFactory.createMoveAction());
+        setPreferredSize(new Dimension(400, 400));
+        addChild(new LabelWidget(getScene(), "Class Diagram 1"));
+        //setBorder(BorderFactory.createLineBorder(4, Color.black)); 
+        setBorder(BorderFactory.createResizeBorder(2, Color.black, false));
+        getActions().addAction(ActionFactory.createResizeAction());
+        getActions().addAction(ActionFactory.createMoveAction(ActionFactory.createSnapToGridMoveStrategy(16, 16), null));
+        // getActions().addAction(ActionFactory.createMoveAction());
         getActions().addAction(ActionFactory.createAcceptAction(new AcceptProvider() {
             @Override
             public ConnectorState isAcceptable(Widget widget, Point point, Transferable t) {
                 return ConnectorState.ACCEPT;
             }
+
             @Override
             public void accept(Widget widget, Point point, Transferable t) {
                 Class<? extends UmlClassElement> droppedClass = (Class<? extends UmlClassElement>) t.getTransferDataFlavors()[2].getRepresentationClass(); // Jako ruzno! Osmisliti kako da izvlacimo iz DataFlavor-a bez gadjanja indeksa!
