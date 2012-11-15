c9e28cbba61b663235bd4692d415cc81c02179d8
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-11-15 23:51:07 +0000

    Otkacena Scena od Container-a, odradjen AtachNodeWidget

---
+++ trunk/nb-uml/UML/UMLModel/src/org/uml/model/Relation.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uml.model;
+
+/**
+ *
+ * @author "NUGS"
+ */
+public class Relation {
+    
+    UmlClassElement src;
+    UmlClassElement trg;
+    
+    public Relation() {
+    }
+    
+    
+}
