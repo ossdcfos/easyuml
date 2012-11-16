695f7aaf47097fb7b6ca572731a7876cde10cbe9
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-11-16 00:50:11 +0000

    Odradjeno iscrtavanje, radi prijem klase na scenu

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/SceneAcceptProvider.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/SceneAcceptProvider.java
@@ -4,12 +4,21 @@
  */
 package org.uml.visual.widgets.actions;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.geom.AffineTransform;
+import java.io.IOException;
+import javax.swing.JComponent;
 import org.netbeans.api.visual.action.AcceptProvider;
 import org.netbeans.api.visual.action.ConnectorState;
 import org.netbeans.api.visual.widget.Widget;
 import org.openide.util.Exceptions;
+import org.openide.util.ImageUtilities;
 import org.uml.model.UmlClassElement;
 import org.uml.visual.widgets.ClassDiagramContainerWidget;
 import org.uml.visual.widgets.ClassDiagramScene;
@@ -30,7 +39,16 @@ public class SceneAcceptProvider implements AcceptProvider{
 
     @Override
     public ConnectorState isAcceptable(Widget widget, Point point, Transferable t) {
-        return ConnectorState.ACCEPT;
+         Image dragImage = getImageFromTransferable(t);
+                JComponent view = classDiagramScene.getView();
+                Graphics2D g2 = (Graphics2D) view.getGraphics();
+                Rectangle visRect = view.getVisibleRect();
+                view.paintImmediately(visRect.x, visRect.y, visRect.width, visRect.height);
+                g2.drawImage(dragImage,
+                        AffineTransform.getTranslateInstance(point.getLocation().getX(),
+                        point.getLocation().getY()),
+                        null);
+                return ConnectorState.ACCEPT;
     }
 
     @Override
@@ -44,6 +62,18 @@ public class SceneAcceptProvider implements AcceptProvider{
                 } catch (IllegalAccessException ex) {
                     Exceptions.printStackTrace(ex);
                 }
+    }
+    
+    private Image getImageFromTransferable(Transferable transferable) {
+        Object o = null;
+        try {
+            o = transferable.getTransferData(DataFlavor.imageFlavor);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (UnsupportedFlavorException ex) {
+            ex.printStackTrace();
+        }
+        return o instanceof Image ? (Image) o : ImageUtilities.loadImage("org/netbeans/shapesample/palette/shape1.png");
     }
     
     
