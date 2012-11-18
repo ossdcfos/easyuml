d48f48a2c533014c728073dc1d880bae7c7b132d
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-11-18 14:09:36 +0000

    Izbaceni svi action provider-i iz ClassWidgeta

---
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/PickMethodModifierAction.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uml.visual.widgets.actions;
+
+import java.awt.Point;
+import org.netbeans.api.visual.action.SelectProvider;
+import org.netbeans.api.visual.widget.Widget;
+import org.uml.visual.widgets.ClassWidget;
+
+/**
+ *
+ * @author "NUGS"
+ */
+public class PickMethodModifierAction implements SelectProvider {
+
+        private int currentImage = 1;
+        ClassWidget classWidget;
+        public PickMethodModifierAction(ClassWidget classWidget) {
+            this.classWidget=classWidget;
+        }
+
+        @Override
+        public boolean isAimingAllowed(Widget widget, Point point, boolean bln) {
+            return true;
+        }
+
+        @Override
+        public boolean isSelectionAllowed(Widget widget, Point point, boolean bln) {
+            return true;
+        }
+
+        @Override
+        public void select(Widget widget, Point point, boolean bln) {
+
+            if (currentImage == 4) {
+                currentImage = 1;
+            } else {
+                currentImage++;
+            }
+            widget.addChild(classWidget.getNextMethodAccessModifier(currentImage));
+
+        }
+    }
