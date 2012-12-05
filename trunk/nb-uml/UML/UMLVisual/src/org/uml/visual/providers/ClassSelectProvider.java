f44382e504a15f2777aef528a1cba25488703bf9
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-12-05 13:40:24 +0000

    Radi brisanje klase na desni klik

---
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/providers/ClassSelectProvider.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uml.visual.providers;
+
+import java.awt.Color;
+import java.awt.Point;
+import org.netbeans.api.visual.action.SelectProvider;
+import org.netbeans.api.visual.widget.Widget;
+
+/**
+ *
+ * @author "NUGS"
+ */
+public class ClassSelectProvider implements SelectProvider{
+
+    @Override
+    public boolean isAimingAllowed(Widget widget, Point point, boolean bln) {
+        return false;
+    }
+
+    @Override
+    public boolean isSelectionAllowed(Widget widget, Point point, boolean bln) {
+        return true;
+    }
+
+    @Override
+    public void select(Widget widget, Point point, boolean bln) {
+        widget.setBackground(Color.WHITE);
+    }
+    
+}
