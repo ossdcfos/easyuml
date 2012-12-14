c3b89138fa2a728089282da7142fc6d5817e6d96
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-12-14 16:42:23 +0000

    Ubacen modifier

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/FieldPopupMenuProvider.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/FieldPopupMenuProvider.java
@@ -14,9 +14,11 @@ import javax.swing.JPopupMenu;
 import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.widget.Widget;
 import org.openide.windows.WindowManager;
+import org.uml.visual.dialogs.ChangeModifierDialog;
 import org.uml.visual.dialogs.ChangeVisibilityDalog;
 import org.uml.visual.widgets.ClassDiagramScene;
 import org.uml.visual.widgets.ClassWidget;
+import org.uml.visual.widgets.UMLWidget;
 
 /**
  *
@@ -28,25 +30,45 @@ public class FieldPopupMenuProvider implements PopupMenuProvider{
     private JPopupMenu menu;
     private JMenuItem deleteField;
     private JMenuItem visibility;
-    JDialog dialog;
+    private JMenuItem modifiers;
+
     
     public FieldPopupMenuProvider(Widget fieldWidget) {
         this.fieldWidget = fieldWidget;
         menu = new JPopupMenu("Class Menu");
+
         (visibility = new JMenuItem("Visibility")).addActionListener(changeVisibility);
         menu.add(visibility);
+        (modifiers = new JMenuItem("Modifiers")).addActionListener(changeModifier);
+        menu.add(modifiers);
+        menu.addSeparator();
         (deleteField = new JMenuItem("Delete Field")).addActionListener(removeWidgetListener);
         menu.add(deleteField);
     }
+    
+    ActionListener changeModifier= new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JDialog dialog = new ChangeModifierDialog(null,fieldWidget,true);
+            dialog.setLocationRelativeTo(WindowManager.getDefault().getMainWindow());
+            dialog.setPreferredSize(new Dimension(100,100));
+            dialog.setResizable(false);
+            dialog.setVisible(true);
+            fieldWidget.getScene().validate();
+            
+        }
+    };
     
     ActionListener changeVisibility = new ActionListener() {
 
         @Override
         public void actionPerformed(ActionEvent e) {
           
-            dialog = new ChangeVisibilityDalog(null,(ClassDiagramScene)fieldWidget.getScene(),true);
+            JDialog dialog = new ChangeVisibilityDalog(null,fieldWidget,true);
             dialog.setLocationRelativeTo(WindowManager.getDefault().getMainWindow());
             dialog.setPreferredSize(new Dimension(100,100));
+            dialog.setResizable(false);
             dialog.setVisible(true);
             fieldWidget.getScene().validate();
             
