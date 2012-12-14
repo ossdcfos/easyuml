3ce38b4233042ae4efbc5e3f63333b3e6978ffe7
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2012-12-14 11:58:57 +0000

    Ubacen Visibility na desni klik na field

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/FieldPopupMenuProvider.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/actions/FieldPopupMenuProvider.java
@@ -4,13 +4,18 @@
  */
 package org.uml.visual.widgets.actions;
 
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JDialog;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.widget.Widget;
+import org.openide.windows.WindowManager;
+import org.uml.visual.dialogs.ChangeVisibilityDalog;
+import org.uml.visual.widgets.ClassDiagramScene;
 import org.uml.visual.widgets.ClassWidget;
 
 /**
@@ -22,13 +27,31 @@ public class FieldPopupMenuProvider implements PopupMenuProvider{
     private Widget fieldWidget;
     private JPopupMenu menu;
     private JMenuItem deleteField;
+    private JMenuItem visibility;
+    JDialog dialog;
     
     public FieldPopupMenuProvider(Widget fieldWidget) {
         this.fieldWidget = fieldWidget;
         menu = new JPopupMenu("Class Menu");
+        (visibility = new JMenuItem("Visibility")).addActionListener(changeVisibility);
+        menu.add(visibility);
         (deleteField = new JMenuItem("Delete Field")).addActionListener(removeWidgetListener);
         menu.add(deleteField);
     }
+    
+    ActionListener changeVisibility = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          
+            dialog = new ChangeVisibilityDalog(null,(ClassDiagramScene)fieldWidget.getScene(),true);
+            dialog.setLocationRelativeTo(WindowManager.getDefault().getMainWindow());
+            dialog.setPreferredSize(new Dimension(100,100));
+            dialog.setVisible(true);
+            fieldWidget.getScene().validate();
+            
+        }
+    };
     
     ActionListener removeWidgetListener = new ActionListener() {
         @Override
