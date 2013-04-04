d3289bed981ff20e69954fd4269a794551b76815
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2013-04-04 15:41:14 +0000

    Brisanje klase brise i relacije.

--- trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/providers/ClassPopupMenuProvider.java
+++ trunk/nb-uml/UML/UMLVisual/src/org/uml/visual/widgets/providers/ClassPopupMenuProvider.java
@@ -8,12 +8,15 @@ import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.util.Map;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import org.netbeans.api.visual.action.ActionFactory;
 import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.action.WidgetAction;
 import org.netbeans.api.visual.widget.Widget;
+import org.uml.model.ClassDiagram;
+import org.uml.model.RelationComponent;
 import org.uml.visual.widgets.ClassWidget;
 import org.uml.visual.widgets.actions.LabelTextFieldEditorAction;
 
@@ -57,6 +60,16 @@ public class ClassPopupMenuProvider implements PopupMenuProvider {
         @Override
         public void actionPerformed(ActionEvent e) {
            classWidget.getComponent().getParentDiagram().removeComponent(classWidget.getClassName());
+           ClassDiagram classDiagram = classWidget.getComponent().getParentDiagram();
+           
+           for(Map.Entry<String,RelationComponent> entry : classDiagram.getRelations().entrySet()) {
+                RelationComponent relation = entry.getValue();
+                if (relation.getSource().getName().equals(classWidget.getClassName())||relation.getTarget().getName().equals(classWidget.getClassName())) {
+                   classDiagram.removeRelation(relation.getName());
+                   classWidget.getClassDiagramScene().removeEdge(relation);
+               }
+           }
+            
            classWidget.removeFromParent();
            
         }
