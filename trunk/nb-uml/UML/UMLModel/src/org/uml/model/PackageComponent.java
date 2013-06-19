80fee4848b1adb3690b942ae4fc964d16200f954
uros_stojkic <uros_stojkic@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2013-06-19 11:42:07 +0000

    ubacen PackageComponent

---
+++ trunk/nb-uml/UML/UMLModel/src/org/uml/model/PackageComponent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uml.model;
+
+/**
+ *
+ * @author Uros
+ */
+public class PackageComponent extends ClassDiagramComponent{
+
+    public PackageComponent() {
+        this.setName("UntitledPackage");
+    }
+
+    public PackageComponent(String name) {
+        super(name);
+    }
+    
+    
+    
+}
