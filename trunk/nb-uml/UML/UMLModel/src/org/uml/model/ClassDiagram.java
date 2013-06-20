b9fd3a3f5b94def9d7784441020b6578f95013b5
sevarac <sevarac@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2013-06-20 17:46:22 +0000

    uml files

--- trunk/nb-uml/UML/UMLModel/src/org/uml/model/ClassDiagram.java
+++ trunk/nb-uml/UML/UMLModel/src/org/uml/model/ClassDiagram.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.uml.model;
 
 import java.io.Serializable;
@@ -11,7 +7,7 @@ import java.util.HashMap;
  *
  * @author Uros
  */
-public class ClassDiagram implements Serializable{
+public class ClassDiagram implements Serializable {
 
     private String name;
     private HashMap<String, ClassDiagramComponent> components; // classes, interfaces or enums
