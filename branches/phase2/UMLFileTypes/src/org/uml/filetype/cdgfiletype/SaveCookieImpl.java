25dd20a9ac95369ce7b4b582dea4d11f5dc58c03
ivail <ivail@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2014-04-01 08:13:43 +0000

    Serialization methods improvements.

--- branches/phase2/UMLFileTypes/src/org/uml/filetype/cdgfiletype/SaveCookieImpl.java
+++ branches/phase2/UMLFileTypes/src/org/uml/filetype/cdgfiletype/SaveCookieImpl.java
@@ -3,12 +3,21 @@ package org.uml.filetype.cdgfiletype;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import javax.rmi.CORBA.Util;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.NotifyDescriptor.Confirmation;
 import org.openide.cookies.SaveCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Exceptions;
+import org.openide.util.Utilities;
+import org.uml.model.ClassDiagram;
+import org.uml.xmlSerialization.ClassDiagramXmlSerializer;
 
 /**
  *
@@ -45,21 +54,30 @@ public class SaveCookieImpl implements SaveCookie {
     
     public void save(CDGDataObject cdg){
         FileOutputStream fileOut = null;
-        ObjectOutputStream out = null;
+        XMLWriter writer = null;
         try {
             FileObject fo = cdg.getPrimaryFile();
             String putanja = fo.getPath();
             fileOut = new FileOutputStream(putanja);
-            out = new ObjectOutputStream(fileOut);
-            out.writeObject(cdg.getClassDiagram());
-            out.close();
-            fileOut.close();
-        } catch (IOException ex) {
+            ClassDiagramXmlSerializer serializer = ClassDiagramXmlSerializer.getInstance();
+            serializer.setClassDiagram(Utilities.actionsGlobalContext().lookup(ClassDiagram.class));
+            Document document = DocumentHelper.createDocument();
+            Element root = document.addElement("ClassDiagram");
+            serializer.serialize(root);
+            OutputFormat format = OutputFormat.createPrettyPrint();
+            writer = new XMLWriter(
+                fileOut, format 
+            );
+            writer.write( document );
+
+            
+            System.out.println("Ispisano u fajl");
+        } catch (Exception ex) {
             Exceptions.printStackTrace(ex);
         } finally {
             try {
                 fileOut.close();
-                out.close();
+                writer.close();
             } catch (IOException ex) {
                 Exceptions.printStackTrace(ex);
             }
