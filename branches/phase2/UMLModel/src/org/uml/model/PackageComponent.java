9034a311777517836a13f9d7858ecd004962f1ff
ivail <ivail@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2014-04-21 23:32:12 +0000

    CodeGenerator form redesigned. Package now knows which components it has and stores them into collection. Added action setPackage for Interfaces. Added action setPackage for Enums.

--- branches/phase2/UMLModel/src/org/uml/model/PackageComponent.java
+++ branches/phase2/UMLModel/src/org/uml/model/PackageComponent.java
@@ -4,18 +4,26 @@
  */
 package org.uml.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Implementation of InterfaceComponent, one of four posible types of ClassDiagramComponents.
+ * Implementation of InterfaceComponent, one of four posible types of
+ * ClassDiagramComponents.
+ *
  * @author zoran
  * @see ClassDiagramComponent
  * @see ClassComponent
  * @see EnumComponent
  * @see InterfaceComponent
  */
-public class PackageComponent extends ClassDiagramComponent{
+public class PackageComponent extends ClassDiagramComponent {
+
+    private HashMap<String, ClassDiagramComponent> classDiagramComponent;
 
     /**
-     * Contructor without parameters which sets PackageComponent's name on "UntitledPackage"
+     * Contructor without parameters which sets PackageComponent's name on
+     * "UntitledPackage"
      */
     public PackageComponent() {
         this.setName("UntitledPackage");
@@ -23,13 +31,38 @@ public class PackageComponent extends ClassDiagramComponent{
 
     /**
      * Constructor with parameter which sets PackageComponent's name
+     *
      * @param name of PackageComponent
      */
     public PackageComponent(String name) {
         super(name);
+        classDiagramComponent = new HashMap<String, ClassDiagramComponent>();
     }
-    
-    
-    
-    
+
+    public HashMap<String, ClassDiagramComponent> getComponents() {
+        return classDiagramComponent;
+    }
+
+    public void addComponent(ClassDiagramComponent cdc) {
+//        if (cdc instanceof ClassComponent) {
+//            cdc = (ClassDiagramComponent) cdc;
+//        }
+        classDiagramComponent.put(cdc.getName(), cdc);
+    }
+
+    public void removeComponent(ClassDiagramComponent cdc) {
+        classDiagramComponent.remove(cdc.getName());
+    }
+
+    public void printAllComponentsOnStdOut() {
+        System.out.println("PackageComponent: " + this.getName());
+        System.out.println("Contains these ClassDiagramComponents: ");
+        int counter = 1;
+        for (Map.Entry<String, ClassDiagramComponent> entry : classDiagramComponent.entrySet()) {
+            String string = entry.getKey();
+            ClassDiagramComponent classComponent = entry.getValue();
+            System.out.println("Component no." + counter + ": \t" + classComponent.getName());
+            counter++;
+        }
+    }
 }
