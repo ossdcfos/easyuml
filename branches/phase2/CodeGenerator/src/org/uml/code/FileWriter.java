fdfc06f7e768f95d6ecf1e458fc3be3f4e4c4f2a
ivail <ivail@f18ebcc5-b3a6-cb74-e67c-8ddd6d1f129b>
2014-05-12 05:46:29 +0000

    Code Generator:
    - Automatic code Indentation and Reformatting implemented for each generated file. Many thanks to Geertjan. :)
    
    IMPORTANT NOTE: Many new dependencies are included, please update with dependencies.

--- branches/phase2/CodeGenerator/src/org/uml/code/FileWriter.java
+++ branches/phase2/CodeGenerator/src/org/uml/code/FileWriter.java
@@ -3,18 +3,28 @@ package org.uml.code;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.util.Arrays;
-import java.util.List;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
 import org.netbeans.api.project.Project;
+import org.netbeans.modules.editor.indent.api.Reformat;
+import org.openide.cookies.EditorCookie;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.text.NbDocument;
 
 /**
  * Provides writing generated code to .java files into project's directory.
  *
  * @author Ilija
  * @version 1.0
+ * @see
+ * https://blogs.oracle.com/geertjan/entry/format_files_while_starting_netbeans
+ * @see
+ * http://bits.netbeans.org/dev/javadoc/org-netbeans-modules-editor-indent/overview-summary.html
  */
 public class FileWriter {
 
@@ -74,6 +84,48 @@ public class FileWriter {
                 System.err.println("Error while closing BufferedWriter.");
             }
         }
+
+//this one was used: https://blogs.oracle.com/geertjan/entry/format_files_while_starting_netbeans1 
+
+
+
+        File oneFileToBeFormatted = new File(longestPath + name + ".java");
+        try {
+            FileObject fo = FileUtil.toFileObject(oneFileToBeFormatted);
+            DataObject dobj = DataObject.find(fo);
+            EditorCookie ec = dobj.getCookie(EditorCookie.class);
+            final StyledDocument doc = ec.openDocument();
+            final Reformat rf = Reformat.get(doc);
+            rf.lock();
+            try {
+                NbDocument.runAtomicAsUser(doc, new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            rf.reformat(0, doc.getLength());
+                        } catch (BadLocationException ex) {
+                        }
+                    }
+                });
+            } finally {
+                rf.unlock();
+            }
+            ec.saveDocument();
+        } catch (BadLocationException ex) {
+        } catch (IOException ex) {
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     public void createDirectory(File file) {
