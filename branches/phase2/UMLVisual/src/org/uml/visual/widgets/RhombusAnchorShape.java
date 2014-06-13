43a1de2603d83a84fc9d988e9b5d2c8e75949c97
Zoran Sevarac <sevarac@gmail.com>
2014-06-13 14:15:12 +0000

    romb

--- branches/phase2/UMLVisual/src/org/uml/visual/widgets/RhombusAnchorShape.java
+++ branches/phase2/UMLVisual/src/org/uml/visual/widgets/RhombusAnchorShape.java
@@ -15,16 +15,20 @@ public class RhombusAnchorShape implements AnchorShape {
     private static final Stroke STROKE = new BasicStroke(1.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
     private GeneralPath path;
     private int size;
+    int degrees;
     
     // http://www.dickbaldwin.com/java/Java310-fig17.htm  kako nacrtati romb
+    // http://www.ehow.com/how_12165313_draw-diamond-java.html
 
     public RhombusAnchorShape(int degrees, int size) {
         this.size = size;
+        this.degrees=degrees;
         path = new GeneralPath();
         double radians = Math.PI * degrees / 180.0;
         double cos = Math.cos(radians / 2.0);
-        double sin = -size * Math.sqrt(1 - cos * cos);
-        cos *= size;
+        double sin = Math.sin(radians / 2.0);
+      //  double sin = -size * Math.sqrt(1 - cos * cos);
+      //  cos *= size;
 
         // strelica
 //        path.moveTo(0.0f, 0.0f);
@@ -37,21 +41,26 @@ public class RhombusAnchorShape implements AnchorShape {
 //        path.lineTo(0.0f*ds,0.5f*ds);
 //        path.lineTo(-0.5f*ds,0.0f*ds);
 //        path.lineTo(0.0f*ds,-0.5f*ds);        
-        size = 2*size;
+       
         // path create romb
         GeneralPath thePath = new GeneralPath();
-        path.moveTo(size, 0.0f*size);
-        path.lineTo(0.0f*size,0.5f*size);
-        path.lineTo(-0.5f*size,0.0f*size);
+        float x1 = size, y1=0;
+        //double x1 = size*sin, y1=size*cos;
+        path.moveTo(x1, y1);
+        float x2 = size * 0.5f, y2 = size*0.5f;
+        path.lineTo(x2, y2);
+        float x3 = 0.0f, y3=0.0f;
+        path.lineTo(x3, y3);
+        float x4 = size*0.5f , y4=-size*0.5f;
+        path.lineTo(0.5*size,-size*0.5);
         
-        path.lineTo(0.0f*size,-0.5f*size);
         path.closePath();        
         
     }
 
     @Override
     public boolean isLineOriented() {
-        return false;
+        return true;
     }
 
     @Override
@@ -73,6 +82,9 @@ public class RhombusAnchorShape implements AnchorShape {
 
         graphics.setColor(Color.BLACK);
         graphics.fill(path);
+        
+        graphics.rotate(Math.toRadians(degrees));
+        
 //        super.paintComponent(graphics);
 
 
