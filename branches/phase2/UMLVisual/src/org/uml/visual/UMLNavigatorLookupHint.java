772e46435098f16cee00b7ac050a12d6a525cc0f
Geertjan Wielenga <g_wielenga@yahoo.com>
2014-06-25 14:59:46 +0000

    Moving the satellite view from the UML designer to the Navigator

---
+++ branches/phase2/UMLVisual/src/org/uml/visual/UMLNavigatorLookupHint.java
@@ -0,0 +1,12 @@
+package org.uml.visual;
+
+import org.netbeans.spi.navigator.NavigatorLookupHint;
+
+public class UMLNavigatorLookupHint implements NavigatorLookupHint {
+
+    @Override
+    public String getContentType() {
+        return "application/uml";
+    }
+    
+}
